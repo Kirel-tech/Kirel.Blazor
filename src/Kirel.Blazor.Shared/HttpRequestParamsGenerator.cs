0d985167c9e11837569efba21a884b59666c14a3
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2023-02-06 11:54:09 +0300

    add shared library with 0.0.4 version
    * Added http request params generator
    
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

---
+++ src/Kirel.Blazor.Shared/HttpRequestParamsGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using MudBlazor;
+
+namespace Kirel.Blazor.Shared;
+
+/// <summary>
+/// Http requests parameters generator for basic requests
+/// </summary>
+public static class HttpRequestParamsGenerator
+{
+    /// <summary>
+    /// Generate parameters for get list requests.
+    /// </summary>
+    /// <param name="state">MudBlazor table state</param>
+    /// <returns>Parameters string</returns>
+    public static string GetListParams(TableState state)
+    {
+        var uriBuilder = new StringBuilder();
+        if (!string.IsNullOrEmpty(state?.SortLabel))
+        {
+            uriBuilder.Append($"&orderBy={state.SortLabel}");
+            uriBuilder.Append(state.SortDirection == SortDirection.Descending ? "&orderDirection=desc" : "&orderDirection=asc");
+        }
+
+        uriBuilder.Append($"&pageNumber={state!.Page + 1}");
+        uriBuilder.Append($"&pageSize={state!.PageSize}");
+        
+        var uriStr = uriBuilder.ToString();
+        return uriStr.Remove(0, 1);
+    }
+}
